71922aa27ddd6e50858d83e40122e65eb949f947
Jonathan Kaufman <jondakauf@gmail.com>
2016-10-19 14:09:07 -0400

    fix production build

--- client/bootstrap/Root.prod.js
+++ client/bootstrap/Root.prod.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
 import { Provider } from 'react-redux';
-import App from './App';
+import App from '../containers/App';
 
 const Root = ({ store, history }) => (
   <Provider store={store}>

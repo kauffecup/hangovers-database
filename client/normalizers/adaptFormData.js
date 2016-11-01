28f59acc64eb46dda91d82f2040a4331a1f3a7b6
Jonathan Kaufman <jondakauf@gmail.com>
2016-11-01 10:07:24 -0400

    only adapt fields that have truthy values

--- client/normalizers/adaptFormData.js
+++ client/normalizers/adaptFormData.js
@@ -56,20 +56,20 @@ export const syllableAdapter = s =>
   s && typeof s === 'boolean' ? 'yes' : 'no';
 
 export const fullArrangementAdapter = (a = {}) => Object.assign({}, a, {
-  active: activeAdapter(a.active),
+  active: a.active && activeAdapter(a.active),
   albums: (a.albums || []).map(albumAdapter),
   arrangers: (a.arrangers || []).map(hangoverAdapter),
-  arrangementType: arrangementTypeAdapter(a.arrangementType),
+  arrangementType: a.arrangementType && arrangementTypeAdapter(a.arrangementType),
   concerts: (a.concerts || []).map(concertAdapter),
   finale: attatchmentAdapter(a._attachments, 'mus'),
-  genre: genreAdapter(a.genre),
-  key: keyAdapter(a.key),
+  genre: a.genre && genreAdapter(a.genre),
+  key: a.key && keyAdapter(a.key),
   mp3: attatchmentAdapter(a._attachments, 'mp3'),
   originalArtists: (a.originalArtists || []).map(artistAdapter),
   pdf: attatchmentAdapter(a._attachments, 'pdf'),
-  quality: qualityAdapter(a.quality),
+  quality: a.quality && qualityAdapter(a.quality),
   soloists: (a.soloists || []).map(hangoverAdapter),
-  syllables: syllableAdapter(a.syllables),
-  whenArranged: semesterAdapter(a.whenArranged),
+  syllables: a.syllables && syllableAdapter(a.syllables),
+  whenArranged: a.whenArranged && semesterAdapter(a.whenArranged),
   whenPerformed: (a.whenPerformed || []).map(semesterAdapter),
 });

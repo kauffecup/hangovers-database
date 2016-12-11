c63febd39613039d36cbcef5f3c0ca7abe681517
Jonathan Kaufman <jondakauf@gmail.com>
2016-12-11 14:13:23 -0500

    finalize relationship types and id generator methods

--- server/cloudantHelpers/IDGenerators.js
+++ server/cloudantHelpers/IDGenerators.js
@@ -1,83 +1,82 @@
 const types = require('./DBTypes');
 
-const getArrangementID = arrangement =>
+module.exports.getArrangementID = arrangement =>
   `${types.ARRANGEMENT_TYPE}_${arrangement.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getArrangementTypeID = arrangementType =>
+module.exports.getArrangementTypeID = arrangementType =>
   `${types.ARRANGEMENT_TYPE_TYPE}_${arrangementType.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getHangoverID = hangover =>
+module.exports.getHangoverID = hangover =>
   `${types.HANGOVER_TYPE}_${hangover.lastName.toLowerCase().replace(/\s/g, '_')}_${hangover.firstName.toLowerCase().replace('.', '').replace(/\s/g, '_')}`;
 
-const getSemesterID = semester =>
+module.exports.getSemesterID = semester =>
   `${types.SEMESTER_TYPE}_${semester.year}_${semester.semester_type.toLowerCase()}`;
 
-const getAlbumID = album =>
+module.exports.getAlbumID = album =>
   `${types.ALBUM_TYPE}_${album.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getAlbumFormatID = albumFormat =>
+module.exports.getAlbumFormatID = albumFormat =>
   `${types.ALBUM_FORMAT_TYPE}_${albumFormat.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getConcertID = concert =>
+module.exports.getConcertID = concert =>
   `${types.CONCERT_TYPE}_${concert.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getConcertTypeID = concertType =>
+module.exports.getConcertTypeID = concertType =>
   `${types.CONCERT_TYPE_TYPE}_${concertType.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getGenreID = genre =>
+module.exports.getGenreID = genre =>
   `${types.GENRE_TYPE}_${genre.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getArtistID = artist =>
+module.exports.getArtistID = artist =>
   `${types.ARTIST_TYPE}_${artist.name.toLowerCase().replace(/\s/g, '_')}`;
 
-const getKeyID = key =>
+module.exports.getKeyID = key =>
   `${types.KEY_TYPE}_${key.name.toLowerCase().replace(/\s/g, '_')}_${key.tonality.toLowerCase().replace(/\s/g, '_')}`;
 
-const getTagID = tag =>
+module.exports.getTagID = tag =>
   `${types.TAG_TYPE}_${tag.name.toLowerCase().replace(/\s/g, '_')}`;
 
 
 // relationship IDs
 
-const getAlbumSemesterID = (album, semester) =>
-  `${types.ALBUM_SEMESTER_RELATIONSHIP_TYPE}_${getAlbumID(album)}_${getSemesterID(semester)}`;
+module.exports.getAlbumSemesterID = (albumID, semesterID) =>
+  `${types.ALBUM_SEMESTER_RELATIONSHIP_TYPE}_${albumID}_${semesterID}`;
 
-const getHangoverGraduationID = (hangover, semester) =>
-  `${types.HANGOVER_GRADUATION_SEMESTER_RELATIONSHIP_TYPE}_${getHangoverID(hangover)}_${getSemesterID(semester)}`;
+module.exports.getArrangementAlbumID = (arrangementID, albumID) =>
+  `${types.ARRANGEMENT_ALBUMS_RELATIONSHIP_TYPE}_${arrangementID}_${albumID}`;
 
-const getBMSemesterID = (hangover, semester) =>
-  `${types.BM_SEMESTER_RELATIONSHIP_TYPE}_${getHangoverID(hangover)}_${getSemesterID(semester)}`;
+module.exports.getArrangementArrangerID = (arrangementID, hangoverID) =>
+  `${types.ARRANGEMENT_ARRANGERS_RELATIONSHIP_TYPE}_${arrangementID}_${hangoverID}`;
 
-const getConcertSemesterID = (concert, semester) =>
-  `${types.CONCERT_SEMESTER_RELATIONSHIP_TYPE}_${getConcertID(concert)}_${getSemesterID(semester)}`;
+module.exports.getArrangementConcertID = (arrangementID, concertID) =>
+  `${types.ARRANGEMENT_CONCERTS_RELATIONSHIP_TYPE}_${arrangementID}_${concertID}`;
 
-const getMDConcertID = (hangover, concert) =>
-`${types.MD_CONCERT_RELATIONSHIP_TYPE}_${getHangoverID(hangover)}_${getConcertID(concert)}`;
+module.exports.getArrangementGenreID = (arrangementID, genreID) =>
+  `${types.ARRANGEMENT_GENRE_RELATIONSHIP_TYPE}_${arrangementID}_${genreID}`;
 
-const getMDSemesterID = (hangover, semester) =>
-  `${types.MD_SEMESTER_RELATIONSHIP_TYPE}_${getHangoverID(hangover)}_${getSemesterID(semester)}`;
+module.exports.getArrangementSemesterArrangedID = (arrangementID, semesterID) =>
+  `${types.ARRANGEMENT_SEMESTER_ARRANGED_RELATIONSHIP_TYPE}_${arrangementID}_${semesterID}`;
 
-const getPresidentSemesterID = (hangover, semester) =>
-  `${types.PRESIDENT_SEMESTER_RELATIONSHIP_TYPE}_${getHangoverID(hangover)}_${getSemesterID(semester)}`;
+module.exports.getArrangementSemesterPerformedID = (arrangementID, semesterID) =>
+  `${types.ARRANGEMENT_SEMESTERS_PERFORMED_RELATIONSHIP_TYPE}_${arrangementID}_${semesterID}`;
 
-// exports
+module.exports.getArrangementSoloistID = (arrangementID, hangoverID) =>
+  `${types.ARRANGEMENT_SOLOISTS_RELATIONSHIP_TYPE}_${arrangementID}_${hangoverID}`;
 
-module.exports.getArrangementID = getArrangementID;
-module.exports.getArrangementTypeID = getArrangementTypeID;
-module.exports.getHangoverID = getHangoverID;
-module.exports.getSemesterID = getSemesterID;
-module.exports.getAlbumID = getAlbumID;
-module.exports.getAlbumFormatID = getAlbumFormatID;
-module.exports.getConcertID = getConcertID;
-module.exports.getConcertTypeID = getConcertTypeID;
-module.exports.getGenreID = getGenreID;
-module.exports.getArtistID = getArtistID;
-module.exports.getKeyID = getKeyID;
-module.exports.getTagID = getTagID;
-module.exports.getAlbumSemesterID = getAlbumSemesterID;
-module.exports.getHangoverGraduationID = getHangoverGraduationID;
-module.exports.getBMSemesterID = getBMSemesterID;
-module.exports.getConcertSemesterID = getConcertSemesterID;
-module.exports.getMDConcertID = getMDConcertID;
-module.exports.getMDSemesterID = getMDSemesterID;
-module.exports.getPresidentSemesterID = getPresidentSemesterID;
+module.exports.getBMSemesterID = (hangoverID, semesterID) =>
+  `${types.BM_SEMESTER_RELATIONSHIP_TYPE}_${hangoverID}_${semesterID}`;
+
+module.exports.getConcertSemesterID = (concertID, semesterID) =>
+  `${types.CONCERT_SEMESTER_RELATIONSHIP_TYPE}_${concertID}_${semesterID}`;
+
+module.exports.getHangoverGraduationID = (hangoverID, semesterID) =>
+  `${types.HANGOVER_GRADUATION_SEMESTER_RELATIONSHIP_TYPE}_${hangoverID}_${semesterID}`;
+
+module.exports.getMDConcertID = (hangoverID, concertID) =>
+`${types.MD_CONCERT_RELATIONSHIP_TYPE}_${hangoverID}_${concertID}`;
+
+module.exports.getMDSemesterID = (hangoverID, semesterID) =>
+  `${types.MD_SEMESTER_RELATIONSHIP_TYPE}_${hangoverID}_${semesterID}`;
+
+module.exports.getPresidentSemesterID = (hangoverID, semesterID) =>
+  `${types.PRESIDENT_SEMESTER_RELATIONSHIP_TYPE}_${hangoverID}_${semesterID}`;

08df6cae3fdc0f079829ae5f58130bd968f8652f
Jonathan Kaufman <jondakauf@gmail.com>
2016-12-25 15:32:31 -0500

    sort semesters albums and concerts by semester urrywhere in the app

--- client/reducers/app.js
+++ client/reducers/app.js
@@ -15,6 +15,16 @@ const initialState = {
   keys: [],
 };
 
+const semesterSort = (a, b) => {
+  if (a.year < b.year) return 1;
+  if (a.year > b.year) return -1;
+  if (a.semester_type === 'spring') return 1;
+  if (a.semester_type === 'fall') return -1;
+  return 0;
+};
+
+const fieldSemesterSort = field => (a, b) => semesterSort(a[field], b[field]);
+
 export default function reduce(state = initialState, action) {
   switch (action.type) {
     case INITIALIZE_FORMS:
@@ -28,9 +38,9 @@ export default function reduce(state = initialState, action) {
         arrangementTypes: action.data.arrangementTypes.rows,
         albumFormats: action.data.albumFormats.rows,
         concertTypes: action.data.concertTypes.rows,
-        semesters: action.data.semesters.rows,
-        albums: action.data.albums.rows,
-        concerts: action.data.concerts.rows,
+        semesters: action.data.semesters.rows.sort(semesterSort),
+        albums: action.data.albums.rows.sort(fieldSemesterSort('semester')),
+        concerts: action.data.concerts.rows.sort(fieldSemesterSort('semester')),
         keys: action.data.keys.rows,
       });
     }
